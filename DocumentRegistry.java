c1ee692552a077774e28d246d1f38ac4b77b62df
Rene Angelo Prado <152822689+RenePrado@users.noreply.github.com>
2025-04-28 00:54:27 +0800

    Create DocumentRegistry.java

--- DocumentRegistry.java
+++ DocumentRegistry.java
@@ -8,7 +8,7 @@ public class DocumentRegistry {
         pdfPrototype = new PdfDocument("annual_report_2024.pdf", "Acme Corp", 150, "Annual Report");
 
         System.out.println("Creating a Text Document prototype.");
-        textDocumentPrototype = new TextDocument("meeting_notes.txt".hashCode(), "UTF-8", 250);
+        textDocumentPrototype = new TextDocument(1, "UTF-8", 250);
 
         System.out.println("Creating a Spreadsheet Document prototype.");
         spreadsheetPrototype = new SpreadsheetDocument("sales_data_q1.xlsx", 1000, 20);
@@ -16,22 +16,22 @@ public class DocumentRegistry {
         System.out.println();
     }
 
-    public Document createPdfCopy(String fileName, int pages) {
+    public Document createPdfDocument(String fileName, int pages) {
         PdfDocument copy = (PdfDocument) pdfPrototype.clone();
         copy.fileName = fileName;
         copy.pageCount = pages;
         return copy;
     }
 
-    public Document getPdfPrototype() {
-        return pdfPrototype;
+    public Document createPdf() {
+        return pdfPrototype.clone();
     }
 
-    public Document getTextDocumentPrototype() {
-        return textDocumentPrototype;
+    public Document createText() {
+        return textDocumentPrototype.clone();
     }
 
-    public Document getSpreadsheetPrototype() {
-        return spreadsheetPrototype;
+    public Document createSpreadsheet() {
+        return spreadsheetPrototype.clone();
     }
 }
